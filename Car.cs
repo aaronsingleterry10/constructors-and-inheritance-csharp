68a09c81efa2436bbd357b05dd91bc3dda299d57
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-28 22:25:51 -0500

    created car and vehicle classes

---
+++ Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConstructorsInheritance
+{
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+            Console.WriteLine("Car is being initialized");
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConstructorsInheritance
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized");
+        }
+    }
+}
